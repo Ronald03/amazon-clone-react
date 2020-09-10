4a1b679e1d2c36d2de04f9fe08a6424970a889d8
RonaldLnxDesk <ronaldceballos15@gmail.com>
2020-09-09 19:51:26 -0500

    Body (Home) completed

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,61 @@
 import React from "react";
+import "./Home.css";
+import Product from "./Product";
 
 function Home() {
-  return <div></div>;
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+          alt=""
+        />
+        <div className="home__row">
+          <Product
+            title="The lean startup "
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            rating={4}
+          />
+          <Product
+            title="Samsung LC429827402 49' Curve LED Gaming Monitor"
+            price={199.99}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+          />
+        </div>
+        <div className="home__row">
+          <Product
+            title="New Apple iPad Pro (12.90inch, Wi-Fi, 125GB) - Silver (4th Gen)"
+            price={199.99}
+            rating={3}
+            image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
+          />
+          <Product
+            title="Samsung LC429827402 49' Curve LED Gaming Monitor"
+            price={199.99}
+            rating={2}
+            image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+          />
+          <Product
+            title="Samsung LC429827402 49' Curve LED Gaming Monitor"
+            price={199.99}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+          />
+        </div>
+        <div className="home__row">
+          <Product
+            title="Samsung LC4556453XEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
+            price={529.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
+            rating={5}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
