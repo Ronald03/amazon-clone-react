9f71e9a0996f97bd4a12c7e4baa650a2e0758c8c
RonaldLnxDesk <ronaldceballos15@gmail.com>
2020-09-17 06:13:05 -0500

    login/register functionality implemented

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { auth } from "./firebase";
 
 function Login() {
+  const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = (e) => {
+    e.preventDefault();
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((auth) => {
+        history.push("/");
+      })
+      .catch((error) => alert(error.message));
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        if (auth) {
+          history.push("/");
+        }
+        console.log(auth);
+      })
+      .catch((error) => alert(error.message));
+  };
   return (
     <div className="login">
       <Link to="/">
@@ -17,16 +45,32 @@ function Login() {
 
         <form action="">
           <h5>E-mail</h5>
-          <input type="text" />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
           <h5>Password</h5>
-          <input type="password" />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-          <button className="login__signInButton">Sign In</button>
+          <button
+            type="submit"
+            className="login__signInButton"
+            onClick={signIn}
+          >
+            Sign In
+          </button>
         </form>
         <p>By Signin-in you agree to the Amazon FAKE CLONE Conditions of Use</p>
 
-        <button className="login__registerButton">Create an Account</button>
+        <button className="login__registerButton" onClick={register}>
+          Create an Account
+        </button>
       </div>
     </div>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCV-dr9kDXviMkxGPcegqBpMYmckj82kvo",
   authDomain: "clone-8aca9.firebaseapp.com",
@@ -7,3 +9,10 @@ const firebaseConfig = {
   messagingSenderId: "814791603055",
   appId: "1:814791603055:web:ea5ad64ad2954b28e92e9a",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
