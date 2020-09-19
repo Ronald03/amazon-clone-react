ddf6120ebaaa5c1bf4b6ae78a5e7f77fc308cfd6
RonaldLnxDesk <ronaldceballos15@gmail.com>
2020-09-19 08:01:49 -0500

    Stripe Payment copmpleted; App processes pay

--- src/App.js
+++ src/App.js
@@ -6,15 +6,20 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Checkout";
 import Login from "./Login";
 import Payment from "./Payment";
+import Orders from "./Orders";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51HSvzOCvJ1HFKVYpg4noLQJR9EaD7K4Bgt2FawSsgfWkajLnZVtBGq7z0xlpIhqvUnTYpYG4U1CzsdiC9aNODaYz00ijPbK6kn"
+);
 
 function App() {
   const [{}, dispatch] = useStateValue();
   useEffect(() => {
     auth.onAuthStateChanged((authUser) => {
-      console.log("THE USER IS >>> ", authUser);
-
       if (authUser) {
         //the user just loffed in / the user was logged in
         dispatch({
@@ -35,6 +40,9 @@ function App() {
       <div className="app">
         <Switch>
           <Route path="/login">
+            <Orders />
+          </Route>
+          <Route path="/orders">
             <Login />
           </Route>
           <Route path="/checkout">
@@ -43,7 +51,9 @@ function App() {
           </Route>
           <Route path="/payment">
             <Header />
-            <Payment />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
           <Route path="/">
             <Header />
